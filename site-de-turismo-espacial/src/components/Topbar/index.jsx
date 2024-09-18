4fa82cc52bd08152c3fc371fcb59477296e34435
Irae Veras <irae.online@gmail.com>
2024-09-18 18:04:47 -0300

    style created styles component topbar

--- site-de-turismo-espacial/src/components/Topbar/index.jsx
+++ site-de-turismo-espacial/src/components/Topbar/index.jsx
@@ -1,15 +1,23 @@
+import "./index.css";
+
 const Topbar = () => {
     return (
-        <div style={{display: "grid", gridTemplateColumns: "repeat(2, 1fr)"}}>
-            <div style={{}}>
-                <a href="http://">Logo</a>
+        <div className="topbar">
+            <div className="container-logo">
+                <div className="content-logo">
+                    <a className="link-logo" href="/"></a>
+                    <span className="line"></span>
+                </div>
             </div>
-            <nav style={{width: "100%", background: "#ccc"}}>
-                <ul style={{width: "100%", display: "flex", alignItems: "center", justifyContent: "space-evenly"}}>
-                    <li><a href="http://">00 Home</a></li>
-                    <li><a href="http://">01 Destination</a></li>
-                    <li><a href="http://">02 Crew</a></li>
-                    <li><a href="http://">Technology</a></li>
+            {/* <button className="btn-hamburger">
+                <img className="menu-hamburger" src="../../assets/shared/icon-hamburger.svg" alt="" />
+            </button> */}
+            <nav className="navigation">
+                <ul className="nav-list">
+                    <li className="active nav-link"><a href="/">00 Home</a></li>
+                    <li className="nav-link"><a href="/destination">01 Destination</a></li>
+                    <li className="nav-link"><a href="http://">02 Crew</a></li>
+                    <li className="nav-link"><a href="http://">Technology</a></li>
                 </ul>
             </nav>
         </div>
