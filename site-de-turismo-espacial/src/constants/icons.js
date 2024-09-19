5a57653f5b47e05c4e0adcb6dc95932f1aef34dc
Irae Veras <irae.online@gmail.com>
2024-09-19 17:11:04 -0300

    feat: import icon image close menu

---
+++ site-de-turismo-espacial/src/constants/icons.js
@@ -0,0 +1,5 @@
+import logo from "../assets/shared/logo.svg";
+import iconMenuHamburguer from "../assets/shared/icon-hamburger.svg";
+import iconMenuClose from "../assets/shared/icon-close.svg";
+
+export default { logo, iconMenuHamburguer, iconMenuClose }
