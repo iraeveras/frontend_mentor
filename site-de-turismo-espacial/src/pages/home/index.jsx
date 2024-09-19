38c38e084fb6dfe0ec0133c557e00afe610acd3b
Irae Veras <irae.online@gmail.com>
2024-09-19 17:10:15 -0300

    feat: created content page home

--- site-de-turismo-espacial/src/pages/home/index.jsx
+++ site-de-turismo-espacial/src/pages/home/index.jsx
@@ -2,7 +2,26 @@ import "./index.css";
 
 const Home = () => {
     return (
-        <div className="home"></div>
+        <div className="home">
+            <div className="wrapper">
+                <div className="wrapper-content">
+                    <div className="space">
+                        <h6>So, you want to travel to</h6>
+                        <h1>Space</h1>
+                        <p>
+                            Let’s face it; if you want to go to space, you might as well genuinely go to
+                            outer space and not hover kind of on the edge of it. Well sit back, and relax
+                            because we’ll give you a truly out of this world experience!
+                        </p>
+                    </div>
+                    <div className="explore">
+                        <div className="explore-button">
+                            <a href="/destination">Explore</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
