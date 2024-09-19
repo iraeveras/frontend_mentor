6658e38ae80e2470f87819effc7bd3eea249b902
Irae Veras <irae.online@gmail.com>
2024-09-19 17:12:04 -0300

    feat: created function open and close menu mobile

--- site-de-turismo-espacial/src/components/Topbar/index.jsx
+++ site-de-turismo-espacial/src/components/Topbar/index.jsx
@@ -1,26 +1,63 @@
+import React, { useState } from "react";
+import icons from "../../constants/icons.js"
 import "./index.css";
 
+
+
 const Topbar = () => {
+    const [openMenuMobile, setOpenMenuMobile] = useState(false)
+    const handleOpeMenuMobile = () => {
+        setOpenMenuMobile(true)
+    }
+
+    const handleCloseMenuMobile = () => {
+        setOpenMenuMobile(false)
+    }
     return (
-        <div className="topbar">
-            <div className="container-logo">
-                <div className="content-logo">
-                    <a className="link-logo" href="/"></a>
-                    <span className="line"></span>
+        <>
+            <div className="topbar">
+                <div className="container-logo">
+                    <div className="content-logo">
+                        <a className="link-logo" href="/">
+                            <img src={icons.logo} alt="" />
+                        </a>
+                        <span className="line"></span>
+                    </div>
                 </div>
+
+                <div className="container-btn-hamburger">
+                    <button onClick={handleOpeMenuMobile} className="btn-hamburger">
+                        <img src={icons.iconMenuHamburguer} alt="" />
+                    </button>
+                </div>
+
+                <nav className="navigation">
+                    <ul className="nav-list">
+                        <li className="active nav-link"><a href="/">00 Home</a></li>
+                        <li className="nav-link"><a href="/destination">01 Destination</a></li>
+                        <li className="nav-link"><a href="http://">02 Crew</a></li>
+                        <li className="nav-link"><a href="http://">Technology</a></li>
+                    </ul>
+                </nav>
             </div>
-            {/* <button className="btn-hamburger">
-                <img className="menu-hamburger" src="../../assets/shared/icon-hamburger.svg" alt="" />
-            </button> */}
-            <nav className="navigation">
+
+            <nav className={openMenuMobile ? "navigation-mobile" : "close-navigation-mobile"}>
+                <div className="container-btn-close">
+                    <div>
+                        <button onClick={handleCloseMenuMobile} className="btn-close"><img src={icons.iconMenuClose} alt="" /></button>
+                    </div>
+
+                </div>
                 <ul className="nav-list">
-                    <li className="active nav-link"><a href="/">00 Home</a></li>
-                    <li className="nav-link"><a href="/destination">01 Destination</a></li>
-                    <li className="nav-link"><a href="http://">02 Crew</a></li>
-                    <li className="nav-link"><a href="http://">Technology</a></li>
+                    <div className="box-links">
+                        <li className="nav-link"><a href="/">00 Home</a></li>
+                        <li className="nav-link"><a href="/destination">01 Destination</a></li>
+                        <li className="nav-link"><a href="http://">02 Crew</a></li>
+                        <li className="nav-link"><a href="http://">03 Technology</a></li>
+                    </div>
                 </ul>
             </nav>
-        </div>
+        </>
     )
 }
 
