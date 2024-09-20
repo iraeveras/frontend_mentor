275b3dff0f86321d92c76d2fce65e06a0ae98782
Irae Veras <irae.online@gmail.com>
2024-09-20 18:08:05 -0300

    feat: change position component topbar

--- site-de-turismo-espacial/src/App.jsx
+++ site-de-turismo-espacial/src/App.jsx
@@ -1,16 +1,22 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Topbar from "./components/Topbar";
 import Home from "./pages/home";
 import Destination from "./pages/destination";
-import Topbar from "./components/Topbar";
+import Crew from "./pages/crew";
+import Technology from "./pages/technology";
+
 function App() {
 
   return (
     <div className='app'>
-      <Topbar />
       <div className="content">
+        <Topbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
         </Routes>
       </div>
     </div>
