bf79d3139feab1fd3719a6ca0e8d0a5ac0b183a5
Irae Veras <irae.online@gmail.com>
2024-09-20 18:10:54 -0300

    feat: change path file index.css global to eliminate issue warnings

--- site-de-turismo-espacial/src/main.jsx
+++ site-de-turismo-espacial/src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
