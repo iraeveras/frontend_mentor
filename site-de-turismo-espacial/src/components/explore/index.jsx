f046d9b3dac4cb7f4786ac0e27d4c0d6a49646f6
iraeveras <irae.online@gmail.com>
2024-09-22 18:35:53 -0300

    feat: created component explore

---
+++ site-de-turismo-espacial/src/components/explore/index.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+const Explore = () => {
+    return (
+        <div className="explore">
+            <div className="explore-button">
+                <a href="/destination">Explore</a>
+            </div>
+        </div>
+    )
+}
+
+export default Explore;
