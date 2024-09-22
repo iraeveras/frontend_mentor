d2cf054b54e81d707713bd78a4b8cbb7ff9a8463
iraeveras <irae.online@gmail.com>
2024-09-22 18:34:26 -0300

    feat: created component Destination

--- site-de-turismo-espacial/src/pages/destination/index.jsx
+++ site-de-turismo-espacial/src/pages/destination/index.jsx
@@ -1,8 +1,56 @@
-import "./index.css"
+import Wrapper from "../../components/wrapper";
+import Wrappercontent from "../../components/wrapper-content"
+import icons from "../../constants/icons";
+import "./index.css";
 const Destination = () => {
     return (
         <div className="destination">
-
+            <Wrapper classNameValue="wrapper-destination">
+                <Wrappercontent classNameValue="wrapper-content-destination">
+                    <div className="title">
+                        <span>01</span>
+                        <h2>pick your destination</h2>
+                    </div>
+                    <section className="body-destination">
+                        <div className="img-explanation">
+                            <div>
+                                <img src={icons.imgMoonWebg} alt="" />
+                            </div>                            
+                        </div>
+                        <div className="content-explanation">
+                            <div>
+                                <nav className="menu-destination">
+                                    <ul>
+                                        <li><button>moon</button></li>
+                                        <li><button>mars</button></li>
+                                        <li><button>europa</button></li>
+                                        <li><button>titan</button></li>
+                                    </ul>
+                                </nav>
+                                <div className="about">
+                                    <h1 className="about-name">MARS</h1>
+                                    <p className="about-text">
+                                        See our planet as you’ve never seen it before. 
+                                        A perfect relaxing trip away to help regain perspective and come 
+                                        back refreshed. While you’re there, take in some history 
+                                        by visiting the Luna 2 and Apollo 11 landing sites.
+                                    </p>
+                                </div>
+                                <footer>
+                                    <div className="distance">
+                                        <span className="descrition-text">avg. distance</span>
+                                        <span className="descrition-number">384,400 km</span>
+                                    </div>
+                                    <div className="time">
+                                        <span className="descrition-text">est. travel time</span>
+                                        <span className="descrition-day">3 days</span>
+                                    </div>
+                                </footer>
+                            </div>
+                        </div>
+                    </section>
+                </Wrappercontent>
+            </Wrapper>
         </div>
     )
 }
