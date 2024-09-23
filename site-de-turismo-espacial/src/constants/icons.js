d1d49b795d0689f8ccb06fa97c0a6fc639342ddf
iraeveras <irae.online@gmail.com>
2024-09-23 00:30:28 -0300

    feat: import imgLine

--- site-de-turismo-espacial/src/constants/icons.js
+++ site-de-turismo-espacial/src/constants/icons.js
@@ -3,5 +3,6 @@ import iconMenuHamburguer from "../assets/shared/icon-hamburger.svg";
 import iconMenuClose from "../assets/shared/icon-close.svg";
 import imgMoonPng from "../assets/destination/image-moon.png";
 import imgMoonWebg from "../assets/destination/image-moon.webp";
+import imgLine from "../assets/shared/line.png";
 
-export default { logo, iconMenuHamburguer, iconMenuClose, imgMoonPng, imgMoonWebg }
+export default { logo, iconMenuHamburguer, iconMenuClose, imgMoonPng, imgMoonWebg, imgLine }
