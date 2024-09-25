8f53ac026bf9a164adc9d3e685bd590efbe63980
Irae Veras <irae.online@gmail.com>
2024-09-25 17:59:17 -0300

    feat: create init page crew

--- site-de-turismo-espacial/src/pages/crew/index.jsx
+++ site-de-turismo-espacial/src/pages/crew/index.jsx
@@ -2,14 +2,40 @@ import Container from "../../components/container";
 import Content from "../../components/content";
 import Topbar from "../../components/Topbar";
 import Wrapper from "../../components/wrapper";
+import icons from "../../constants/icons";
 import "./index.css"
 const Crew = () => {
     return (
         <Container classNameValue="container crew">
             <Content>
-                <Topbar/>
+                <Topbar />
                 <Wrapper>
-                    <div className="content-crew"></div>
+                    <div className="content-crew">
+                        <div className="crew-title">
+                            <span>02</span>
+                            <p>MEET YOUR CREW</p>
+                        </div>
+                        <section className="crew-body-explanation">
+                            <div className="crew-body-explanation-left">
+                                <div className="profile">
+                                    <div className="description-profile">
+                                        <p className="role">Commander</p>
+                                        <p className="name">Douglas Hurley</p>
+                                    </div>
+                                    <p className="bio">
+                                        Douglas Gerald Hurley is an American engineer, former Marine Corps
+                                        pilot and former NASA astronaut. He launched into space for the third
+                                        time as commander of Crew Dragon Demo-2.
+                                    </p>
+                                </div>
+                                <div className="pagination"></div>
+                            </div>
+                            <div className="crew-body-explanation-right">
+                                <img src={icons.imgDouglasHurleyPng} alt="" />
+                            </div>
+
+                        </section>
+                    </div>
                 </Wrapper>
             </Content>
         </Container>
