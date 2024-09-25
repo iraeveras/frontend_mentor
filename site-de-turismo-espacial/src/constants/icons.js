832e651dfe7bcce6a7146e8e291ee64e10aec6ee
Irae Veras <irae.online@gmail.com>
2024-09-25 17:58:35 -0300

    feat: import images

--- site-de-turismo-espacial/src/constants/icons.js
+++ site-de-turismo-espacial/src/constants/icons.js
@@ -4,5 +4,16 @@ import iconMenuClose from "../assets/shared/icon-close.svg";
 import imgMoonPng from "../assets/destination/image-moon.png";
 import imgMoonWebg from "../assets/destination/image-moon.webp";
 import imgLine from "../assets/shared/line.png";
+import imgDouglasHurleyPng from "../assets/crew/image-douglas-hurley.png";
+import imgDouglasHurleyWebp from "../assets/crew/image-douglas-hurley.webp";
 
-export default { logo, iconMenuHamburguer, iconMenuClose, imgMoonPng, imgMoonWebg, imgLine }
+export default {
+    logo,
+    iconMenuHamburguer,
+    iconMenuClose,
+    imgMoonPng,
+    imgMoonWebg,
+    imgLine,
+    imgDouglasHurleyPng,
+    imgDouglasHurleyWebp
+}
