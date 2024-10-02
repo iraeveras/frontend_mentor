f339444a9dd068817101e2ad3705bd0ce106c94e
Irae Veras <irae.online@gmail.com>
2024-10-02 18:19:11 -0300

    feat: init page technology

--- site-de-turismo-espacial/src/pages/technology/index.jsx
+++ site-de-turismo-espacial/src/pages/technology/index.jsx
@@ -7,9 +7,20 @@ const Technology = () => {
     return (
         <Container classNameValue="container technology">
             <Content>
-                <Topbar/>
+                <Topbar />
                 <Wrapper>
-                    <div className="content-technology"></div>
+                    <div className="content-technology">
+                        <div className="technology-title">
+                            <span>03</span>
+                            <p>SPACE LAUNCH 101</p>
+                        </div>
+                        <section className="technology-body-explanation">
+                            <div className="tchnology-body-explanation-left">
+
+                            </div>
+                            <div className="tchnology-body-explanation-right"></div>
+                        </section>
+                    </div>
                 </Wrapper>
             </Content>
         </Container>
