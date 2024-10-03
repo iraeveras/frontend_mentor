6fd2e0bc5ef61ff9014446542e1507555cdf57de
Irae Veras <irae.online@gmail.com>
2024-10-03 18:10:11 -0300

    feat: building the technology page

--- site-de-turismo-espacial/src/pages/technology/index.jsx
+++ site-de-turismo-espacial/src/pages/technology/index.jsx
@@ -2,6 +2,7 @@ import Container from "../../components/container";
 import Content from "../../components/content";
 import Topbar from "../../components/Topbar";
 import Wrapper from "../../components/wrapper";
+import icons from "../../constants/icons";
 import "./index.css"
 const Technology = () => {
     return (
@@ -10,16 +11,51 @@ const Technology = () => {
                 <Topbar />
                 <Wrapper>
                     <div className="content-technology">
-                        <div className="technology-title">
-                            <span>03</span>
-                            <p>SPACE LAUNCH 101</p>
-                        </div>
-                        <section className="technology-body-explanation">
-                            <div className="tchnology-body-explanation-left">
-
+                        <div className="container-technology">
+                            <div className="technology-title">
+                                <span>03</span>
+                                <p>SPACE LAUNCH 101</p>
                             </div>
-                            <div className="tchnology-body-explanation-right"></div>
-                        </section>
+                            <section className="technology-body-explanation">
+                                <div className="technology-body-explanation-left">
+                                    <div className="pagination-explanation-buttons">
+                                        <div className="box-btn-tchnology">
+                                            <button className="btn btn-index-1">1</button>
+                                        </div>
+                                        <div className="box-btn-tchnology">
+                                            <button className="btn btn-index-2">2</button>
+                                        </div>
+                                        <div className="box-btn-tchnology">
+                                            <button className="btn btn-index-3">3</button>
+                                        </div>
+                                    </div>
+                                    <div className="technology-content-explanation">
+                                        <div className="classification">
+                                            <div className="classification-name">
+                                                THE TERMINOLOGY…
+                                            </div>
+                                            <div className="classification-commander">
+                                                LAUNCH VEHICLE
+                                            </div>
+                                        </div>
+                                        <div className="classification-description">
+                                            A launch vehicle or carrier rocket is a rocket-propelled vehicle
+                                            used to carry a payload from Earth's surface to space, usually to
+                                            Earth orbit or beyond. Our WEB-X carrier rocket is the most
+                                            powerful in operation. Standing 150 metres tall, it's quite an
+                                            awe-inspiring sight on the launch pad!
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="technology-body-explanation-right">
+                                    <div className="box-technology-image">
+                                        <div className="technology-image">
+                                            <img src={icons.imgLounchVehiclePortrait} alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </Wrapper>
             </Content>
