0328da9839a464b62d6518027ad5fed2c04df379
iraeveras <irae.online@gmail.com>
2024-10-15 23:14:46 -0300

    style: refactoration styles response page home

--- site-de-turismo-espacial/src/components/space/index.jsx
+++ site-de-turismo-espacial/src/components/space/index.jsx
@@ -4,11 +4,11 @@ const Space = () => {
         <div className="space">
             <h6>So, you want to travel to</h6>
             <h1>Space</h1>
-            <p>
+            <blockquote>
                 Let’s face it; if you want to go to space, you might as well genuinely go to
                 outer space and not hover kind of on the edge of it. Well sit back, and relax
                 because we’ll give you a truly out of this world experience!
-            </p>
+            </blockquote>
         </div>
     )
 }
